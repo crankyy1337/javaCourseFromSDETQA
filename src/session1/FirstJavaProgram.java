f3129fe2cb9b4d20c23f8c319781bbf5e9b4d8af
crankyy1337 <muradeligeorge@gmail.com>
2024-10-07 23:18:52 +0300

    create master branch

---
+++ src/session1/FirstJavaProgram.java
@@ -0,0 +1,9 @@
+package session1;
+
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println(10+20);
+        System.out.println("10+20");
+    }
+}
---
+++ src/session1/SecondJavaProgram.java
@@ -0,0 +1,13 @@
+package session1;
+
+public class SecondJavaProgram {
+    public static void main(String[] args) {
+        /*
+        System.out.println("hello...1");
+        System.out.println("hello...2");
+        System.out.println("hello...3");
+        System.out.println("hello...4");
+         */
+
+    }
+}
